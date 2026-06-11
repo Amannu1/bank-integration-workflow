c01d6687037e217370956c87103f6e71bcc2244a
Luiz <manuercolle@gmail.com>
2026-06-11 14:45:41 -0300

    feat(user, bank): restrict API endpoints by role

--- src/main/java/com/luizercole/bankworkflow/resources/BankResource.java
+++ src/main/java/com/luizercole/bankworkflow/resources/BankResource.java
@@ -5,6 +5,7 @@ import com.luizercole.bankworkflow.services.BankService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -18,18 +19,21 @@ public class BankResource {
     @Autowired
     private BankService bankService;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @GetMapping
     public ResponseEntity<List<BankDTO>> findAll(){
         List<BankDTO> list = bankService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @GetMapping("/{id}")
     public ResponseEntity<BankDTO> findById(@PathVariable Long id){
         BankDTO dto = bankService.findById(id);
         return ResponseEntity.ok().body(dto);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @PostMapping
     public ResponseEntity<BankDTO> createBank(@Valid @RequestBody BankDTO bankDTO){
         bankDTO = bankService.createBank(bankDTO);
@@ -37,12 +41,14 @@ public class BankResource {
         return ResponseEntity.created(uri).body(bankDTO);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @PutMapping("/{id}")
     public ResponseEntity<BankDTO> updateBank(@PathVariable Long id, @RequestBody @Valid BankDTO bankDTO){
         bankDTO = bankService.updateBank(id, bankDTO);
         return ResponseEntity.ok().body(bankDTO);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @DeleteMapping("/{id}")
     public ResponseEntity<BankDTO> deleteBank(@PathVariable Long id){
         bankService.deleteBank(id);
