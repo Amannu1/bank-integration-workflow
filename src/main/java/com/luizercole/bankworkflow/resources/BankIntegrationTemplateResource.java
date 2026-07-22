406a65dd607ce99691341f3babc53f4db9cf3b6b
Luiz <manuercolle@gmail.com>
2026-07-22 10:45:11 -0300

    feat: findAll, findById and create Bank Integration Template Resource and Service

---
+++ src/main/java/com/luizercole/bankworkflow/resources/BankIntegrationTemplateResource.java
@@ -0,0 +1,41 @@
+package com.luizercole.bankworkflow.resources;
+
+import com.luizercole.bankworkflow.dto.BankIntegrationTemplateDTO;
+import com.luizercole.bankworkflow.entities.BankIntegrationTemplate;
+import com.luizercole.bankworkflow.services.BankIntegrationTemplateService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/bank-integration-templates")
+public class BankIntegrationTemplateResource {
+
+    @Autowired
+    private BankIntegrationTemplateService bankIntegrationTemplateService;
+
+    @GetMapping
+    public ResponseEntity<List<BankIntegrationTemplateDTO>> findAll(){
+        List<BankIntegrationTemplateDTO> list = bankIntegrationTemplateService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BankIntegrationTemplateDTO> findById(@PathVariable Long id){
+        BankIntegrationTemplateDTO dto = bankIntegrationTemplateService.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<BankIntegrationTemplateDTO> createBankIntegrationDTO(@Valid @RequestBody BankIntegrationTemplateDTO bankIntegrationTemplateDTO){
+        bankIntegrationTemplateDTO = bankIntegrationTemplateService.createBankIntegrationTemplate(bankIntegrationTemplateDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(bankIntegrationTemplateDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(bankIntegrationTemplateDTO);
+
+    }
+}
